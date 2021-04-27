16cba7f8a7c69ad76af3c621a317361f9cef2f52
damie <damie@DESKTOP-7PVDS72>
2021-04-27 09:42:31 -0500

    fileDeletion class started

---
+++ Gazebo/src/fileDeletion.java
@@ -0,0 +1,39 @@
+/*import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class fileDeletion {
+	
+	//Constructor for fileDelection
+	fileDeletion() {
+		try {
+			File anomalies_cblof = new File("anomalies_cblof.csv");
+			File Anomalies = new File("Anomalies.csv");
+			File busiestTimes = new File("busiestTimes.csv");
+			File copyLogData = new File("copyLogData.csv");
+			File finalAnomCounts = new File("finalAnomCounts.csv");
+			File finalDateCounts = new File("finalDateCounts.csv");
+			File normalizedData = new File("normalizedData.csv");
+			File nullValues = new File("nullValues.csv");
+			File stillThere = new File("stillThere.csv");
+			
+			anomalies_cblof.delete();
+			Anomalies.delete();
+			busiestTimes.delete();
+			copyLogData.delete();
+			finalAnomCounts.delete();
+			finalDateCounts.delete();
+			normalizedData.delete();
+			nullValues.delete();
+			stillThere.delete();
+			
+		} catch {
+			
+		}
+
+		
+		
+	}//CLOSE CONSTRUCTOR
+
+}
+*/
